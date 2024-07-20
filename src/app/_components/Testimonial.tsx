a17103ced98d0497069c0995b456834ea7ac852d
Dane Becker <danebecker790@gmail.com>
2024-07-20 13:45:26 -0500

    Fixed aws images

--- src/app/_components/Testimonial.tsx
+++ src/app/_components/Testimonial.tsx
@@ -19,8 +19,6 @@ type testimonialsType = {
 // ]
 
 export function Testimonial() {
-
-  console.log('test')
   
     return (
       <section className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:px-8">
--- src/app/aws/GetObject.ts
+++ src/app/aws/GetObject.ts
@@ -2,21 +2,9 @@ import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 export async function getImage(key: string) {
-  const s3Client = new S3Client({
-    region: process.env.AWS_REGION ?? 'default-region',
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? 'default-access-key-id',
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? 'default-secret-access-key'
-    }
-  });
-
+  const region = process.env.AWS_REGION ?? 'default-region';
   const bucketName = process.env.BUCKET_NAME;
 
-  // Create a signed URL that expires after a set period, e.g., 60 minutes
-  const url = await getSignedUrl(s3Client, new GetObjectCommand({
-    Bucket: bucketName,
-    Key: key,
-  }), { expiresIn: 3600 });
-
+   const url = `https://${bucketName}.s3.${region}.amazonaws.com/${encodeURIComponent(key)}`
   return url;
 }
